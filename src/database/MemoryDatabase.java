0ea6d4961a15ca6d44ecca03cdb5814096c07e24
Cartaloop <lucasrech00@gmail.com>
2024-06-19 18:10:07 -0300

    Create memory database

--- src/database/MemoryDatabase.java
+++ src/database/MemoryDatabase.java
@@ -6,10 +6,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MemoryDatabase {
-    private List<Person> persons;
+    private final List<Person> persons;
 
     public MemoryDatabase() {
         persons = new ArrayList<>();
+    }
+
+    public String addPerson(Person person) {
+        persons.add(person);
+        return person.toString();
+    }
+
+    public List<Person> getPersons() {
+        if(persons.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return persons;
+    }
+
+    public void removePersonByName(String name) {
+        if(persons.isEmpty()) {
+            System.out.println("Persons list is empty");
+        } else if (name.isEmpty()) {
+            System.out.println("Person name is empty");
+        }
+
+        persons.removeIf(e -> e.getNome().contains(name));
     }
 
 }
