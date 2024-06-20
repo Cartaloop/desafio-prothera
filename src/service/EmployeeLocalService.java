687d76f08f3ab258f490a64d532c52bf38fb00b2
Cartaloop <lucasrech00@gmail.com>
2024-06-20 00:01:41 -0300

    remove unecessary methods

--- src/service/EmployeeLocalService.java
+++ src/service/EmployeeLocalService.java
@@ -2,9 +2,6 @@ package service;
 
 import database.MemoryDatabase;
 import model.Employee;
-import model.Person;
-
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.*;
@@ -14,40 +11,6 @@ import static tools.StaticTools.dateFormater;
 
 public class EmployeeLocalService {
     private final MemoryDatabase memoryDatabase = new MemoryDatabase();
-
-
-    public void addEmployee(Employee employee) {
-        memoryDatabase.addPerson(employee);
-    }
-
-    public void addManyEmployees(int quantity) {
-        Person[] persons = new Person[quantity];
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < quantity; i++) {
-
-            String name;
-            String birthDate;
-            BigDecimal salary;
-            String role;
-
-            System.out.println("Digite o nome da pessoa: ");
-            name = scanner.next();
-            System.out.println("Digite o aniversário da pessoa: ");
-            birthDate = scanner.next();
-            System.out.println("Digite o salario da pessoa: ");
-            salary = scanner.nextBigDecimal();
-            System.out.println("Digite a função da pessoa: ");
-            role = scanner.next();
-
-            Person employee = new Employee(name, birthDate, salary, role);
-            persons[i] = employee;
-
-        }
-        scanner.close();
-        for (int i = 0; i < quantity; i++) {
-            memoryDatabase.addPerson((Employee) persons[i]);
-        }
-    }
 
     public List<Employee> getAllEmployees() {
         return memoryDatabase.getPersons();
